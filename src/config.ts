9bd294d3caf4e44156cbcf3f9212064d61e692c6
bayu <bayu@DESKTOP-3CT1B4E>
2025-04-18 16:56:16 +0700

    update

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // src/config.ts
-const API_BASE_URL = 'http://127.0.0.1:8000/api';
+const API_BASE_URL = 'https://orderlist.tokosale.com/api';
 
 export const API_ENDPOINTS = {
   LOGIN: `${API_BASE_URL}/login`,
